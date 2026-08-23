459f9291a259f4858d8710386d98343ff604ad5b
yair <yair999r@gmail.com>
2026-08-23 15:21:42 +0300

    create endpoint get/ games/:id

--- routers/router.js
+++ routers/router.js
@@ -9,11 +9,21 @@ router.post("/game", validPlayerName, async (req, res, next) => {
     try {
         const newPlayer = req.body.playerName
         const newGame = await myControler.createNewGame(newPlayer)
-        console.log(newGame)
-        res.status(201).json({newGame})
+        res.status(201).json({...newGame})
     } catch (error) {
         next(error)
     }
 })
+
+router.get("/games/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const exsistsGame = await myControler.getSaveGame(id)
+        res.status(200).json({...exsistsGame})
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 export default router
